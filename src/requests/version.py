ce4b95bd0ed46cfb072a972db6f3042a5efa5ee8
Mateusz 'mat' Rumian <mateusz.rumian@gmail.com>
2017-10-16 21:36:11 +0200

    Added expected return code

--- src/requests/version.py
+++ src/requests/version.py
@@ -1,13 +1,14 @@
+from requests import codes as CODE
+
 from src.requests.session import Session
-from src.utils import *
+from src.request_utils import *
 
 
 class Version(Session):
-
     def __init__(self):
         super().__init__()
         self.session = Session().session
         self.address = Session().address
 
     def get_version(self):
-        get(self.session, self.address, '/version')
+        get(self.session, self.address, '/version', CODE.ok)
