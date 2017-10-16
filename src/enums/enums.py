794811ad50010af123dfda255b097de4958c86cf
Mateusz 'mat' Rumian <mateusz.rumian@gmail.com>
2017-10-16 21:04:00 +0200

    Enums into one file

--- src/enums/enums.py
+++ src/enums/enums.py
@@ -1,9 +1,19 @@
 from enum import Enum
 
 
+class ProjectStatus(Enum):
+    opened = 'opened'
+    closed = 'closed'
+
+
 class DataLinkType(Enum):
     DLT_ATM_RFC1483 = 'DLT_ATM_RFC1483'
     DLT_EN10MB = 'DLT_EN10MB'
     DLT_FRELAY = 'DLT_FRELAY'
     DLT_C_HDLC = 'DLT_C_HDLC'
     DLT_PPP_SERIAL = 'DLT_PPP_SERIAL'
+
+
+class LinkType(Enum):
+    ETHERNET = 'ethernet'
+    SERIAL = 'serial'
--- src/enums/project_status.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-
-class ProjectStatus(Enum):
-    OPEN = 'open'
-    CLOSED = 'closed'
