d7035ea71cdfc605fee98d4876e1c594a8676c0a
Mateusz 'mat' Rumian <mateusz.rumian@gmail.com>
2017-10-16 23:12:32 +0200

    PoC update

--- gieenesczy.py
+++ gieenesczy.py
@@ -1,19 +1,38 @@
+from src.requests.session import Session
 from src.requests.project import Project as ProjectRequests
-from src.structures.project import Project
+from src.requests.node import Node as NodeRequests
+
+from src.structures.node import Node as NodeStruct
+from src.structures.project import Project as ProjectStruct
 
 import logging as log
-
+import uuid
 
 log.basicConfig(level=log.DEBUG)
 
+
+# Create session
+session = Session()
+
 # Create Project instance
-project = ProjectRequests()
+project = ProjectRequests(session)
+
+rand = str(uuid.uuid4())[0:6]
 
 # Set Input data for Project
-project_data = Project(name='demo_name')
+project_data = ProjectStruct(name=rand)
 
 # Create Project
 project.create_project(project_data.json)
 
 # Get Project UUID
 project_uuid = project.get_project_uuid()
+
+# Create Node instance
+node = NodeRequests(session)
+
+# Node data
+node_data = NodeStruct(project_id=project_uuid, name='testowe')
+
+# Create Node
+node.create_node(node_data.json)
