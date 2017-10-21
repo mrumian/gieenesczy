8cfe6b0926a801f73209796cfeb6aa260e38a832
Mateusz 'mat' Rumian <mateusz.rumian@gmail.com>
2017-10-21 20:53:56 +0200

    Pass project_id in constructor

--- src/requests/node.py
+++ src/requests/node.py
@@ -10,14 +10,15 @@ class Node(Session):
     node_data = None
     nodes_list = None
 
-    def __init__(self, session):
+    def __init__(self, session, project_id):
         super().__init__()
         self.session = session.session
         self.address = session.address
+        self.project_id = project_id
 
     def create_node(self, data):
         log_action('Create node')
 
-        link = '/projects/%s/nodes' % data['project_id']
+        link = '/projects/%s/nodes' % self.project_id
 
         self.node_data = post(self.session, self.address, link, data, CODE.created)
